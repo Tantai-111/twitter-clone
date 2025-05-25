3d410580df685eeaf7d27e765bdd1925f1e5dfc5
Tantai-111 <tantaisomya@gmail.com>
2025-05-25 14:30:11 +0700

    1-auth controllers added

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,17 @@ import express from "express";
 import authRouters from "./routes/auth.routes.js"
 import dotenv from "dotenv"
 import connectMangoDB from "./db/connectMondoDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
-console.log(process.env.MONGO_URI)
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use(cookieParser())
 
 app.use("/api/auth", authRouters)
 
