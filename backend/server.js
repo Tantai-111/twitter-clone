94394c1bde07a9f79ac269d2afd3d6bc7c9d4ba6
Tantai-111 <tantaisomya@gmail.com>
2025-06-08 11:47:13 +0700

    1-user routes and controller added

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 import express from "express";
 import authRouters from "./routes/auth.routes.js"
+import userRouters from "./routes/user.routes.js"
 import dotenv from "dotenv"
 import connectMangoDB from "./db/connectMondoDB.js";
 import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
 
 dotenv.config()
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
 
 const app = express()
 const PORT = process.env.PORT || 5000
@@ -15,6 +23,7 @@ app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 
 app.use("/api/auth", authRouters)
+app.use("/api/users", userRouters)
 
 
 app.listen(PORT, () => {
